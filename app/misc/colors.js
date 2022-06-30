e1e00d15cf82ac0b6e111ecbe0267369f8c30046
Matheus1007 <94841019+Matheus1007@users.noreply.github.com>
2022-06-29 21:07:43 -0300

    first commit

---
+++ app/misc/colors.js
@@ -0,0 +1,9 @@
+//Aqui so colocamos as cores, primeiro usamos a palavra chave padrao de exportação para exportar este objeto, dentro desse
+//objeto, temos esses pares de valores-chave que são simplesmente corres.
+
+export default {
+  DARK: '#1e1e1e',
+  LIGHT: '#ffffff',
+  PRIMARY: '#ffdd73',
+  ERROR: '#ff0000',
+};
